08f073f5e3049070b0eaac7010e34491d74a430e
Zairo Bastos <zairo_vianahd@hormail.com>
2025-05-15 12:06:04 -0300

    feat: :zap: Arquivos de criação do banco e schemas criados

---
+++ database/create_database.py
@@ -0,0 +1,46 @@
+import sqlite3
+from contextlib import closing
+from sqlite3 import Cursor, Connection
+from schema_tables import TABLE_SCHEMA_ETTH
+
+
+def create_table(cursor: Cursor, table_name: str) -> None:
+  """
+  Cria uma tabela no banco de dados com o nome especificado, utilizando o schema definido.
+
+  Args:
+    cursor (Cursor): Cursor do banco de dados.
+    table_name (str): Nome da tabela a ser criada.
+  """
+  try:
+    print(f"[INFO] Criando a tabela '{table_name}'...")
+    cursor.execute(TABLE_SCHEMA_ETTH.format(table_name=table_name))
+    print(f"[SUCCESS] Tabela '{table_name}' criada com sucesso.")
+  except sqlite3.Error as e:
+    print(f"[ERROR] Falha ao criar a tabela '{table_name}': {e}")
+    raise
+
+
+def create_database(db_path: str = './database/database.db') -> None:
+  """
+  Cria um banco de dados SQLite e as suas tabelas definidas no schema.
+
+  Args:
+    db_path (str, optional): Caminho do arquivo do banco de dados. Defaults to './database/database.py''.
+  """
+  print(f"[INFO] Inicializando criação do banco de dados em '{db_path}'...")
+  try:
+    with closing(sqlite3.connect(db_path)) as conn:
+      with closing(conn.cursor()) as cursor:
+        for table in ['etth1', 'etth2']:
+          create_table(cursor, table)
+      conn.commit()
+    print("[SUCCESS] Banco de dados e tabelas criados com sucesso.")
+  except sqlite3.Error as e:
+    print(f"[ERROR] Falha ao criar o banco de dados: {e}")
+    raise
+
+
+if __name__ == "__main__":
+  print("[DEBUG] Schema da tabela:")
+  create_database()
---
+++ database/schema_tables.py
@@ -0,0 +1,19 @@
+TABLE_SCHEMA_ETTH = """
+CREATE TABLE IF NOT EXISTS {table_name} (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  data_inicio TEXT,
+  data_fim TEXT,
+  horas INTEGER,
+  modelo TEXT,
+  temperatura_modelo REAL,
+  prompt TEXT,
+  tipo_prompt TEXT CHECK(tipo_prompt IN ('ZS', 'FS', 'CoT','CoT+FS')),
+  valores_exatos TEXT,
+  valores_previstos TEXT,
+  smape REAL,
+  top_k INTEGER,
+  max_tokens INTEGER,
+  total_tokens_saida INTEGER,
+  total_tokens_entrada INTEGER,
+  total_tokens INTEGER  
+)"""

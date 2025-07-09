5d8c62f26b6a66c3578e3a8bb03109590ade20bc
Wesley Barbosa <wesley.barbosa.developer@gmail.com>
2025-07-09 10:12:36 -0300

    fix: :bug: Impede leitura de dataset inexistente

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ class Main:
 
 	def run(self):
 		os.makedirs("data", exist_ok=True)
+		os.makedirs("database", exist_ok=True)
+		open('./database/modelos.txt', 'a').close()
 		subprocess.run(["streamlit", "run", "app.py"])
 
 if __name__ == "__main__":
